8e7d1f7221d127c9a568e5dc293bc043aa28bbaa
Sejal Kardam <88422234+sejalkardam@users.noreply.github.com>
2024-01-25 03:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
-
-# MySQL Workbench connection details
 db_config = {
     'user': 'sql6679460',
     'password': 'TvM8EaUPTm',
@@ -14,11 +12,14 @@ db_config = {
     'raise_on_warnings': True
 }
 
-# Create a MySQL connection
+
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
 
-# Function to execute a MySQL query and fetch results
+@app.route('/')
+def hello_world():
+    return 'This is assignment'
+
 def execute_query(query, params=None, fetchone=False):
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor(dictionary=True)
@@ -35,7 +36,7 @@ def execute_query(query, params=None, fetchone=False):
 
     return result
 
-# Display current week leaderboard (Top 200)
+
 @app.route('/leaderboard/current_week', methods=['GET'])
 def current_week_leaderboard():
     start_date = datetime.now() - timedelta(days=datetime.now().weekday())
@@ -44,7 +45,7 @@ def current_week_leaderboard():
     leaderboard = execute_query(query, params)
     return jsonify(leaderboard)
 
-# Display last week leaderboard for a given country (Top 200)
+
 @app.route('/leaderboard/last_week', methods=['GET'])
 def last_week_leaderboard():
     country_code = request.args.get('country_code')
@@ -56,7 +57,7 @@ def last_week_leaderboard():
     start_date = end_date - timedelta(days=6)
     print(f"start_date: {start_date}, end_date: {end_date}, country_code: {country_code}")
 
-    query = "SELECT * FROM assignment.user WHERE timestamp BETWEEN %s AND %s AND country = %s ORDER BY score DESC LIMIT 200"
+    query = "SELECT * FROM user WHERE timestamp BETWEEN %s AND %s AND country = %s ORDER BY score DESC LIMIT 200"
 
 
     # query = "SELECT * FROM assignment.user WHERE timestamp >= %s AND timestamp <= %s AND country = %s ORDER BY score DESC LIMIT 200"
@@ -72,15 +73,15 @@ def user_rank():
     if not uid:
         return jsonify({'error': 'User ID is required'}), 400
 
-    # query = "SELECT `RANK`() OVER (ORDER BY score DESC) AS rank FROM leaderboard WHERE uid = %s"
-    query = """
-    SELECT 
-        (SELECT COUNT(*) + 1
-         FROM assignment.user AS l2
-         WHERE l2.score > l1.score) AS user_rank
-    FROM assignment.user AS l1
-    WHERE uid = %s
-"""
+    query = "SELECT `RANK`() OVER (ORDER BY score DESC) AS rank FROM leaderboard WHERE uid = %s"
+#     query = """
+#     SELECT 
+#         (SELECT COUNT(*) + 1
+#          FROM user AS l2
+#          WHERE l2.score > l1.score) AS user_rank
+#     FROM user AS l1
+#     WHERE uid = %s
+# """
 
     params = (uid,)
     result = execute_query(query, params, fetchone=True)
@@ -95,9 +96,7 @@ if __name__ == '__main__':
 
 
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
